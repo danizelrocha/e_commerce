49ebb9c2fcc58ac48a0b9edb6c2e6ec0f2dc9dd0
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-09-12 15:47:00 -0300

    Componentes
    Pasta componentes nav header
    pages home

--- loja/src/app/app.module.ts
+++ loja/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './core/componente/nav/nav.component';
+import { HeaderComponent } from './core/componente/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
