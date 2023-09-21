205b00069f3fd2c8510ec66415c1525385a53ef8
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-09-21 14:09:23 -0300

    Carrossel_ts
    TS- carrossel e Img

--- loja/src/app/pages/home/home.component.ts
+++ loja/src/app/pages/home/home.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  slides = [
+    { image: '~/../assets/img/Banner infantil.png', alt: 'Banner infantil' },
+    { image: '~/../assets/img/Banner Moda Masculina.png', alt: 'Banner Moda Masculina' },
+    { image: '~/../assets/img/Banner-Moda-Feminina.jpg', alt: 'Banner-Moda-Feminina' }
+  ];
+
+  activeSlideIndex = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Inicializar o carrossel
+    this.showSlide(this.activeSlideIndex);
+  }
+
+  showSlide(index: number) {
+    this.activeSlideIndex = index;
+  }
+
+  nextSlide() {
+    if (this.activeSlideIndex < this.slides.length - 1) {
+      this.activeSlideIndex++;
+    } else {
+      this.activeSlideIndex = 0;
+    }
+    this.showSlide(this.activeSlideIndex);
+  }
+
+  prevSlide() {
+    if (this.activeSlideIndex > 0) {
+      this.activeSlideIndex--;
+    } else {
+      this.activeSlideIndex = this.slides.length - 1;
+    }
+    this.showSlide(this.activeSlideIndex);
+  }
 }
