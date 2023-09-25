458b1ac1587c8b76a9fba2c917dd41a29b8977c9
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-09-25 20:09:17 -0300

    Limpa
    Limpando o código

--- loja/src/app/app.component.ts
+++ loja/src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <app-nav></app-nav>
+  <router-outlet></router-outlet>
+  <app-footer></app-footer>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
